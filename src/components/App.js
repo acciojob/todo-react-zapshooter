a00b198ee1f7b50bc6ec4cee7964061341a3dfd2
zapshooter <92568875+zapshooter@users.noreply.github.com>
2025-10-04 19:33:34 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,47 @@
+import React, { useState } from "react";
+import Todo from "./Todo";
+import "../styles/App.css";
 
-import React from "react";
-import './../styles/App.css';
+function App() {
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState([]);
 
-const App = () => {
+  const handleAddTodo = () => {
+    if (task.trim() !== "") {
+      setTodos([...todos, task]);
+      setTask("");
+    }
+  };
+
+  const handleDelete = (index) => {
+    const updated = todos.filter((_, i) => i !== index);
+    setTodos(updated);
+  };
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div id="main">
+      <input
+        id="taskInput"
+        type="text"
+        value={task}
+        placeholder="Enter a task"
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button id="addBtn" onClick={handleAddTodo}>
+        Add Todo
+      </button>
+
+      <ul>
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            text={todo}
+            onDelete={() => handleDelete(index)}
+          />
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
